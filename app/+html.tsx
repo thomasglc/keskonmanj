29cf92bb1ce03ef403f7d033229ecd434b3d146f
Thomas <thomas.galocha@hotmail.fr>
2026-06-28 18:11:40 +0200

    remove zoom

--- app/+html.tsx
+++ app/+html.tsx
@@ -9,7 +9,7 @@ export default function Root({ children }: PropsWithChildren) {
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no, viewport-fit=cover"
         />
 
         {/* PWA manifest */}
