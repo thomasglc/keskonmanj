3272cd3eb57c9c592a1095216a139362f078d217
Thomas <thomas.galocha@hotmail.fr>
2026-06-28 17:28:45 +0200

    ios

---
+++ app/+html.tsx
@@ -0,0 +1,28 @@
+import { ScrollViewStyleReset } from 'expo-router/html'
+import type { PropsWithChildren } from 'react'
+
+export default function Root({ children }: PropsWithChildren) {
+  return (
+    <html lang="fr">
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover"
+        />
+
+        {/* iOS PWA standalone mode */}
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="apple-mobile-web-app-title" content="Kèskonmanj" />
+
+        {/* Android PWA */}
+        <meta name="mobile-web-app-capable" content="yes" />
+
+        <ScrollViewStyleReset />
+      </head>
+      <body>{children}</body>
+    </html>
+  )
+}
